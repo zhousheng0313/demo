250ce45e36b2b710345fa14f33f18cfd90e6031e
zhousheng0313 <zhousheng1993@outlook.com>
2019-08-08 08:56:57 +0800

    数据库密码加密

--- src/test/java/com/zs/demo/DemoApplicationTests.java
+++ src/test/java/com/zs/demo/DemoApplicationTests.java
@@ -1,16 +1,27 @@
 package com.zs.demo;
 
+import org.jasypt.encryption.StringEncryptor;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
+    @Autowired
+    StringEncryptor stringEncryptor;
 
     @Test
     public void contextLoads() {
+
+    }
+
+    @Test
+    public void encryptPwd() {
+        String result = stringEncryptor.encrypt("root");
+        System.out.println(result);
     }
 
 }
